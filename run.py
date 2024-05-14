32494be433aff703edb5ad1ebf45dbb97a495f03
hamza farahat <122169132+farahathamza@users.noreply.github.com>
2024-05-14 19:51:53 +0100

    ADD: run roote

--- run.py
+++ run.py
@@ -10,15 +10,22 @@ web_app = Flask(__name__)
 
 @web_app.route('/')
 def index():
+    return "Web App is Live!"
+
+
+@web_app.route('/run')
+def index():
+    threading.Thread(target=web_app.run, args=()).start()
+
+    app.start()
+    print("I'm live")
+    
+    idle()
+    app.stop()
+    
     return "Bot is Live!"
     
 
 
-threading.Thread(target=web_app.run, args=()).start()
 
-app.start()
-print("I'm live")
-
-idle()
-app.stop()
 
